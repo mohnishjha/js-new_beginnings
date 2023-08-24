21700b66ac0a84f89af9bf7de440dee28236b589
Shoukoushi <124565394+mohnishjha@users.noreply.github.com>
2023-08-24 13:59:24 +0530

    Update new_test.js
    brought new changes to test file. Merge, commit new and merge again

--- new_test.js
+++ new_test.js
@@ -5,3 +5,5 @@ console.log("Woahh I am learning Git and it's hub!")
 console.log("Created a new branch for the first time and I feel good!")
 
 Console.log("a new test branch was created to check how branches can be made from non-main branches)")
+
+Console.log("The test branch has been successfully merged into the main branch. And now I am also making a new change in test branch to try commiting and merging again") ")
