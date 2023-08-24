66e6751eb1e25381fe013afa15f359e88b66c167
Shoukoushi <124565394+mohnishjha@users.noreply.github.com>
2023-08-24 13:38:24 +0530

    Update new_test.js
    created a new branch for the first time!

--- new_test.js
+++ new_test.js
@@ -1,3 +1,5 @@
 console.log("A new test after a small hiccup has been performed!")
 
 console.log("Woahh I am learning Git and it's hub!")
+
+console.log("Created a new branch for the first time and I feel good!")
