4bac6819d6d91bd6e461b384f05317292325316c
Shoukoushi <124565394+mohnishjha@users.noreply.github.com>
2023-08-24 13:41:22 +0530

    Update new_test.js
    A test branch was created

--- new_test.js
+++ new_test.js
@@ -3,3 +3,5 @@ console.log("A new test after a small hiccup has been performed!")
 console.log("Woahh I am learning Git and it's hub!")
 
 console.log("Created a new branch for the first time and I feel good!")
+
+Console.log("a new test branch was created to check how branches can be made from non-main branches)")
